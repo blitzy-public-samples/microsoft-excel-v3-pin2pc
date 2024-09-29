fea02a85d19b0e9876ae828ede07f3cfeb6252bf
Siddhant P. Pardeshi <siddhantpp@gmail.com>
2024-09-29 18:44:02 +0530

    Add public-samples/microsoft-excel-v3-pin2pc/src/desktop/ExcelDesktop/Properties/AssemblyInfo.cs

---
+++ src/desktop/ExcelDesktop/Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Microsoft Excel Desktop")]
+[assembly: AssemblyDescription("Desktop version of Microsoft Excel for Windows")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft Corporation")]
+[assembly: AssemblyProduct("Microsoft Excel")]
+[assembly: AssemblyCopyright("Copyright © Microsoft Corporation 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a newly generated GUID")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+// TODO: Human tasks
+// 1. Generate a new GUID for the assembly
+// 2. Verify and update the AssemblyVersion and AssemblyFileVersion
